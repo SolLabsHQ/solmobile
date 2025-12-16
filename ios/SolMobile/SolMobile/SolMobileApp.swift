a5ca75d10c9a7187dfe60896b07270bd2e73e20f
Jassen McNulty <jmcnulty25@gmail.com>
2025-12-15 22:51:34 -0800

    Add iOS Xcode project (track files, not submodule)

---
+++ ios/SolMobile/SolMobile/SolMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolMobileApp.swift
+//  SolMobile
+//
+//  Created by Jassen A. McNulty on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SolMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/SolMobile/SolMobileUITests/SolMobileUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SolMobileUITestsLaunchTests.swift
+//  SolMobileUITests
+//
+//  Created by Jassen A. McNulty on 12/15/25.
+//
+
+import XCTest
+
+final class SolMobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
